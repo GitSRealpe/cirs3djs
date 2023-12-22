9667fa43990e4f0a730fb15d883690832ed4fcae
GitSRealpe <msn-realpe@hotmail.com>
2023-12-22 19:52:43 +0100

    testing transform controls

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,56 +6,12 @@ import * as ROS3D from './ros3d/ros3d'
 import { sayHello } from './hello-world';
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { TransformControls } from 'three/examples/jsm/controls/TransformControls';
 import Stats from 'three/examples/jsm/libs/stats.module'
 
-import { GridStack } from 'gridstack'
+import { initMenu } from './menu/panel';
 
-var grid = GridStack.init({
-    handle: '.widget-header',
-    column: 1,
-    cellHeight: 200,
-    float: false,
-    minRow: 3,
-    removable: true
-});
-
-grid.addWidget({
-    // w: 6,
-    // h: 1,
-    content: '<div class="widget-header"><button id="1">x</button></div> \
-            <div class= "widget"> 1</div>'
-});
-
-grid.addWidget({
-    // w: 2,
-    content: '<button id="2">x</button><br>2'
-});
-
-grid.addWidget({
-    // w: 2,
-    content: '<button id="3">x</button><br>2'
-});
-
-grid.addWidget({
-    // w: 2,
-    content: '<button id="3">x</button><br>2'
-});
-
-grid.addWidget({
-    // w: 2,
-    content: '<button id="3">x</button><br>2'
-});
-
-document.getElementById("1")?.addEventListener("click", function (event) {
-    console.log("sss")
-    console.log(this.parentElement.parentElement.parentElement)
-    document.getElementById("menus").getElementsByClassName("grid-stack-element")
-    // @ts-ignore
-    grid.removeWidget(document.getElementById("menus").firstChild)
-    // grid.removeWidget(this.parentElement.parentElement.parentElement)
-})
-
-
+initMenu()
 
 sayHello()
 
@@ -64,7 +20,17 @@ var ros = new ROSLIB.Ros({
     // url: 'ws://192.168.1.172:9090'
 });
 
+// Setup a client to listen to TFs.
+var tfClient = new ROSLIB.TFClient({
+    ros: ros,
+    angularThres: 0.01,
+    transThres: 0.01,
+    rate: 50.0,
+    fixedFrame: '/world'
+});
+
 // initViewer();
+
 
 var ancho = document.getElementById("gui")?.getBoundingClientRect().width
 var alto = document.getElementById("gui")?.getBoundingClientRect().height
@@ -103,7 +69,7 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(ancho, alto)
 document.getElementById("gui")?.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement)
+var orbitControls = new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry(0.3, 0.3, 0.3)
 const material = new THREE.MeshBasicMaterial({
@@ -114,14 +80,23 @@ const material = new THREE.MeshBasicMaterial({
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-// Setup a client to listen to TFs.
-var tfClient = new ROSLIB.TFClient({
-    ros: ros,
-    angularThres: 0.01,
-    transThres: 0.01,
-    rate: 50.0,
-    fixedFrame: '/world'
-});
+const geom = new THREE.SphereGeometry(0.5)
+const mat = new THREE.MeshNormalMaterial()
+
+const sphere = new THREE.Mesh(geom, mat)
+scene.add(sphere)
+
+const transformControls = new TransformControls(camera, renderer.domElement)
+transformControls.setMode("translate")
+transformControls.attach(sphere)
+scene.add(transformControls)
+
+transformControls.addEventListener('dragging-changed', function (event) {
+    orbitControls.enabled = !event.value
+    //dragControls.enabled = !event.value
+})
+
+
 
 var urdfClient = new ROS3D.UrdfClient(
     ros,
